147c54944a42b2298fc4f93ef7a796974debc183
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-10-03 12:52:09 -0300

    Importação Projeto base

---
+++ compras-service/src/test/java/br/com/arcelino/comprasservice/ComprasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.comprasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
